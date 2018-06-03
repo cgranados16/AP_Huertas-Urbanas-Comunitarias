e0f39b44ba9c19996b11b433323c71ce8d312a3a
cgranados16 <cgranados16@gmail.com>
2018-06-03 13:08:54 -0600

    Orden de seeds

--- CultivosCR/database/seeds/DatabaseSeeder.php
+++ CultivosCR/database/seeds/DatabaseSeeder.php
@@ -18,10 +18,15 @@ class DatabaseSeeder extends Seeder
         //     // DistrictSeeder::class,
             
         // ]);
+
         //factory(App\Models\User::class, 500)->create(); //crea 150 usuarios
+
         //factory(App\Models\Garden::class, 150)->create(); //crea colaboradores
-        // factory(App\Models\CollaboratorsPerGarden::class, 500)->create(); //crea colaboradores
+
+        // factory(App\Models\CollaboratorsPerGarden::class, 500)->create(); //crea colaboradore
+        
         //factory(App\Models\Review::class, 500)->create(); //crea opiniones
-        factory(App\Models\PhotosPerGarden::class, 300)->create();
+
+        //factory(App\Models\PhotosPerGarden::class, 300)->create();
     }
 }
